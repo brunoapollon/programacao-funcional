b7ad6cc8be1c079f31411fee5d39f323f8d16e9a
Bruno Lopes <eusoubruno7@gmail.com>
2021-07-06 18:26:16 -0300

    add: topic-12 files

--- atv-topico-12/topico-12/GerandoFuncoes.hs
+++ atv-topico-12/topico-12/GerandoFuncoes.hs
@@ -11,15 +11,12 @@ gerador2 = iterate verif 1
         verif x | x > 0 =  (-x - 1)
                 |  otherwise = (-x + 1)
 
+
 gerador3 = [2^n | n <- [0..]]
 gerador3v2 = iterate (*2) 1
 
-listaVerf t = unfoldr (\b -> if b == 0 then Nothing else Just (b, b `div` 2)) t
 
-gerador4 n = reverse (filter(\x -> x /= 0) [verif n x | x <- lista n] )
-        where
-          lista j = takeWhile (< j+1) (iterate (+1) 0)
-          verif j k = if k `elem` listaVerf j then k else 0
+gerador4 = takeWhile(\y -> y > 0) . iterate(\x -> x `div` 2)
 
 
 gerador5 n = unfoldr (\b -> if b == 0 then Nothing else Just (b, b `div` 2)) n
